45ac1655eca9fa2c12383f18f605e30528b363cd
Dan Dev <peterzen.dev@gmail.com>
2019-08-27 23:01:57 -0400

    Add pages and functionality

--- src/app/events.service.ts
+++ src/app/events.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from 'selenium-webdriver/http';
+import { EventResponse } from './interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventsService {
+  private endpoint = 'https://us-central1-ps-notify-api.cloudfunctions.net/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<EventResponse> {
+  }
+
+  private getByRoute<T>(route: string): Observable<T> {
+  }
 }
